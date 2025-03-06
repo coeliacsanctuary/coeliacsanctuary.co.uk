b8089cd67afab0416fd44ab98db8297ce058097c
Jamie Peters <jamie@jamie-peters.co.uk>
2025-03-06 20:51:45 +0000

    Add horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,14 +2,10 @@
 
 declare(strict_types=1);
 
-use App\Providers\AppServiceProvider;
-use App\Providers\HttpClientServiceProvider;
-use App\Providers\NovaServiceProvider;
-use App\Providers\StripeServiceProvider;
-
 return [
-    AppServiceProvider::class,
-    NovaServiceProvider::class,
-    HttpClientServiceProvider::class,
-    StripeServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\HttpClientServiceProvider::class,
+    App\Providers\NovaServiceProvider::class,
+    App\Providers\StripeServiceProvider::class,
 ];
