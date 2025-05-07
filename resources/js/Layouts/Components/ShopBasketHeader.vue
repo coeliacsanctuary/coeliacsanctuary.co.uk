431a38eb0a5402c8e770e081a0cf3edef2cdb1fa
Jamie Peters <jamie@jamie-peters.co.uk>
2025-05-07 19:09:13 +0100

    Fix #163

--- resources/js/Layouts/Components/ShopBasketHeader.vue
+++ resources/js/Layouts/Components/ShopBasketHeader.vue
@@ -14,6 +14,16 @@ const openBasket = () => {
   EventBus.$emit('open-basket');
 };
 
+const totalItems = computed(() => {
+  let total = 0;
+
+  page.props.basket?.items.forEach((item) => {
+    total += item.quantity;
+  });
+
+  return total;
+});
+
 EventBus.$on('product-added-to-basket', () => {
   router.reload({
     only: ['basket'],
@@ -30,8 +40,8 @@ EventBus.$on('product-added-to-basket', () => {
       class="mx-auto flex max-w-8xl flex-col items-center justify-between p-2 xs:flex-row sm:p-4"
     >
       <div class="prose prose-lg xl:prose-xl">
-        You have <strong v-text="items.length" /> <span v-text="label" /> in
-        your basket
+        You have <strong v-text="totalItems" /> <span v-text="label" /> in your
+        basket
       </div>
       <div
         class="prose prose-lg cursor-pointer font-semibold hover:text-primary-dark xl:prose-xl"
