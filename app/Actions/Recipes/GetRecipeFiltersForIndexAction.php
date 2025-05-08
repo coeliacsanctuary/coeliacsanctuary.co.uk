2c42139663469596eca7340eb2b2cb5e7bb10971
Jamie Peters <jamie@jamie-peters.co.uk>
2025-05-08 21:04:34 +0100

    Fix #161

--- app/Actions/Recipes/GetRecipeFiltersForIndexAction.php
+++ app/Actions/Recipes/GetRecipeFiltersForIndexAction.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Actions\Recipes;
 
 use App\Contracts\Recipes\FilterableRecipeRelation;
-use App\Models\Recipes\Recipe;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
@@ -25,41 +24,21 @@ class GetRecipeFiltersForIndexAction
         $mealFilters = array_filter($filters['meals'] ?? []);
         $freeFromFilters = array_filter($filters['freeFrom'] ?? []);
 
-        /** @var Builder<TFilter> $query */
-        $query = $relation::query(); /** @phpstan-ignore-line */
-        if (count($featureFilters) > 0) {
-            $query->hasRecipesWithFeatures($featureFilters);
-        }
-
-        if (count($mealFilters) > 0) {
-            $query->hasRecipesWithMeals($mealFilters);
-        }
-
-        if (count($freeFromFilters) > 0) {
-            $query->hasRecipesWithFreeFrom($freeFromFilters);
-        }
-
-        /** @var Builder<TFilter> $query */
-        $query = $query->withCount(['recipes' => function (Builder $builder) use ($featureFilters, $mealFilters, $freeFromFilters): Builder { /** @phpstan-ignore-line */
-            /** @var Builder<Recipe> $builder */
-            if (count($featureFilters) > 0) {
-                $builder->hasFeatures($featureFilters);
-            }
-
-            if (count($mealFilters) > 0) {
-                $builder->hasMeals($mealFilters);
-            }
-
-            if (count($freeFromFilters) > 0) {
-                $builder->hasFreeFrom($freeFromFilters);
-            }
-
-            return $builder;
-        }]);
-
         $column = Str::of(class_basename($relation))->after('Recipe')->lower()->toString();
 
+        /** @var Builder<TFilter> $query */
+        $query = $relation::query();  /** @phpstan-ignore-line */
+
         return $query
+            ->when(count($featureFilters) > 0, fn (Builder $query) => $query->hasRecipesWithFeatures($featureFilters))
+            ->when(count($mealFilters) > 0, fn (Builder $query) => $query->hasRecipesWithMeals($mealFilters))
+            ->when(count($freeFromFilters) > 0, fn (Builder $query) => $query->hasRecipesWithFreeFrom($freeFromFilters))
+            ->withCount([
+                'recipes' => fn (Builder $builder): Builder => $builder
+                    ->when(count($featureFilters) > 0, fn (Builder $query): Builder => $query->hasFeatures($featureFilters))/** @phpstan-ignore-line */
+                    ->when(count($mealFilters) > 0, fn (Builder $query): Builder => $query->hasMeals($mealFilters))/** @phpstan-ignore-line */
+                    ->when(count($freeFromFilters) > 0, fn (Builder $query): Builder => $query->hasFreeFrom($freeFromFilters)),/** @phpstan-ignore-line */
+            ])
             ->orderBy($column)
             ->get()
             ->map(fn (FilterableRecipeRelation $relation) => $relation->only([$column, 'slug', 'recipes_count']));
