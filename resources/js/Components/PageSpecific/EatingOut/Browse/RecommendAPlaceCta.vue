81c26c63b24d2643d835bb86df6b206717072ee9
Jamie Peters <jamie@jamie-peters.co.uk>
2025-07-10 13:54:18 +0100

    Fix #244

--- resources/js/Components/PageSpecific/EatingOut/Browse/RecommendAPlaceCta.vue
+++ resources/js/Components/PageSpecific/EatingOut/Browse/RecommendAPlaceCta.vue
@@ -5,11 +5,11 @@ import { PlusIcon } from '@heroicons/vue/24/solid';
 
 <template>
   <Link
-    class="group absolute bottom-0 left-2 z-10 flex items-center p-4 select-none md:p-6"
+    class="group absolute bottom-0 left-2 z-10 flex items-center"
     href="/wheretoeat/recommend-a-place"
   >
     <div
-      class="relative -ml-3 cursor-pointer rounded-full border-2 border-white bg-secondary p-3 text-white shadow-sm transition md:shadow-lg"
+      class="relative m-4 ml-1 cursor-pointer rounded-full border-2 border-white bg-secondary p-3 text-white shadow-sm transition md:m-6 md:shadow-lg"
     >
       <PlusIcon
         class="h-8 w-8 xmd:h-14 xmd:w-14 md:max-xmd:h-12 md:max-xmd:w-12"
@@ -17,7 +17,7 @@ import { PlusIcon } from '@heroicons/vue/24/solid';
     </div>
 
     <div
-      class="ml-2 max-w-[150px] rounded-full border-2 border-white bg-secondary px-4 py-1 text-center text-sm leading-none font-semibold uppercase opacity-0 transition-all duration-300 group-hover:opacity-70 group-hover:delay-500 xmd:text-base"
+      class="pointer-events-none absolute left-[4.5rem] max-w-[150px] rounded-full border-2 border-white bg-secondary px-4 py-1 text-center text-sm leading-none font-semibold uppercase opacity-0 transition-all duration-300 group-hover:opacity-70 group-hover:delay-500 xmd:text-base"
     >
       Recommend a Place
     </div>
