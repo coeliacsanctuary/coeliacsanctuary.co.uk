1bea4157a5ba61c05fee7ca34547191f5f10c2c4
Jamie Peters <jamie@jamie-peters.co.uk>
2025-08-04 20:08:49 +0100

    improve homepage hover groups ux

--- resources/js/Components/PageSpecific/Home/HomeHoverItem.vue
+++ resources/js/Components/PageSpecific/Home/HomeHoverItem.vue
@@ -8,34 +8,36 @@ defineProps<{ item: HomeHoverItemType }>();
 </script>
 
 <template>
-  <Card
-    :shadow="false"
-    class="transform transition duration-500 sm:scale-95 sm:group-hover:opacity-50 sm:hover:scale-105 sm:hover:opacity-100!"
-  >
-    <Link
-      :href="item.link"
-      class="group -m-4"
-      prefetch
+  <div class="group/child h-full w-full">
+    <Card
+      :shadow="false"
+      class="transform transition duration-500 sm:scale-95 sm:group-hover:opacity-50 sm:group-hover/child:scale-105 sm:group-hover/child:opacity-100!"
     >
-      <img
-        v-if="
-          item.type !== 'Recipe' ||
-          (item.type === 'Recipe' && item.square_image)
-        "
-        :alt="item.title"
-        :src="item.image"
-        loading="lazy"
-      />
-      <RecipeSquareImage
-        v-else
-        :alt="item.title"
-        :src="item.image"
-      />
+      <Link
+        :href="item.link"
+        class="group -m-4"
+        prefetch
+      >
+        <img
+          v-if="
+            item.type !== 'Recipe' ||
+            (item.type === 'Recipe' && item.square_image)
+          "
+          :alt="item.title"
+          :src="item.image"
+          loading="lazy"
+        />
+        <RecipeSquareImage
+          v-else
+          :alt="item.title"
+          :src="item.image"
+        />
 
-      <h2
-        class="sm:text-md p-2 text-center text-lg font-semibold transition xs:max-sm:text-xl sm:group-hover:text-primary-dark"
-        v-text="item.title"
-      />
-    </Link>
-  </Card>
+        <h2
+          class="sm:text-md p-2 text-center text-lg font-semibold transition xs:max-sm:text-xl sm:group-hover/child:text-primary-dark"
+          v-text="item.title"
+        />
+      </Link>
+    </Card>
+  </div>
 </template>
