3b90d34d52cefe725d0124523c9712c0355991b4
Jamie Peters <jamie@jamie-peters.co.uk>
2025-08-13 22:06:30 +0100

    clean up users table

---
+++ database/migrations/2025_08_13_210226_remove_redundant_user_table_columns.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table): void {
+            $table->dropForeign('users_user_level_id_foreign');
+
+            $table->dropColumn('phone');
+            $table->dropColumn('user_level_id');
+            $table->dropColumn('last_logged_in_at');
+            $table->dropColumn('last_visited_at');
+            $table->dropColumn('welcome_valid_until');
+        });
+    }
+};
