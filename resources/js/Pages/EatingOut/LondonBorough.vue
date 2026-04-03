6bea16f4eb2d232a4e05a6677dbe333c1e2659fc
Jamie Peters <jamie@jamie-peters.co.uk>
2026-04-03 21:56:17 +0100

    london searching and sorting

--- resources/js/Pages/EatingOut/LondonBorough.vue
+++ resources/js/Pages/EatingOut/LondonBorough.vue
@@ -6,11 +6,34 @@ import { LondonBoroughPage } from '@/types/EateryTypes';
 import TownHeading from '@/Components/PageSpecific/EatingOut/Town/TownHeading.vue';
 import CountyTown from '@/Components/PageSpecific/EatingOut/County/CountyTown.vue';
 import JumpToContentButton from '@/Components/JumpToContentButton.vue';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+import FormInput from '@/Components/Forms/FormInput.vue';
+import FormSelect from '@/Components/Forms/FormSelect.vue';
+import { FormSelectOption } from '@/Components/Forms/Props';
 
-defineProps<{ borough: LondonBoroughPage }>();
+const props = defineProps<{ borough: LondonBoroughPage }>();
 
 const areaList = ref<HTMLElement | null>(null);
+const areaSearch = ref('');
+
+const sortOptions = ref<FormSelectOption[]>([
+  { label: 'Alphabetically', value: 'alphabetical' },
+  { label: 'Total Eateries', value: 'eateries' },
+]);
+
+const currentSort = ref('alphabetical');
+
+const filteredAreas = computed(() => {
+  const areas = props.borough.areas.filter((area) =>
+    area.name.toLowerCase().includes(areaSearch.value.toLowerCase()),
+  );
+
+  if (currentSort.value === 'eateries') {
+    return [...areas].sort((a, b) => b.total_eateries - a.total_eateries);
+  }
+
+  return areas;
+});
 </script>
 
 <template>
@@ -56,15 +79,38 @@ const areaList = ref<HTMLElement | null>(null);
     code="5284484376"
   />
 
-  <div
-    ref="areaList"
-    class="group grid gap-3 md:grid-cols-3"
-  >
-    <CountyTown
-      v-for="area in borough.areas"
-      :key="area.name"
-      :town="area"
-    />
+  <div ref="areaList">
+    <Card class="mx-4 mb-4">
+      <div class="flex items-center justify-between">
+        <FormInput
+          v-model="areaSearch"
+          name="search"
+          label=""
+          :placeholder="`Search for an area in ${borough.name}...`"
+          hide-label
+          borders
+          class="w-full max-w-md"
+        />
+
+        <FormSelect
+          v-model="currentSort"
+          name="sort"
+          :options="sortOptions"
+          label="Sort by"
+          borders
+          class="flex items-center space-x-2 xs:flex-col xs:items-start xs:space-x-0 sm:flex-row sm:items-center sm:space-x-2"
+          size="small"
+        />
+      </div>
+    </Card>
+
+    <div class="group grid gap-3 px-4 md:grid-cols-3">
+      <CountyTown
+        v-for="area in filteredAreas"
+        :key="area.name"
+        :town="area"
+      />
+    </div>
   </div>
 
   <JumpToContentButton
