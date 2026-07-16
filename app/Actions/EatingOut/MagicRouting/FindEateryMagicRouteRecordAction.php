3b4a0c18af6b32a85c2e3e1095123a2539e36c6a
Jamie Peters <jamie@jamie-peters.co.uk>
2026-07-16 21:44:26 +0100

    Update find record action

--- app/Actions/EatingOut/MagicRouting/FindEateryMagicRouteRecordAction.php
+++ app/Actions/EatingOut/MagicRouting/FindEateryMagicRouteRecordAction.php
@@ -17,45 +17,9 @@ class FindEateryMagicRouteRecordAction
 {
     public function handle(EateryMagicRouteType $type, string $rawLocation, ?callable $withConfiguration = null): EateryMagicRouteRecord
     {
-        $record = EateryMagicRouteRecord::query()
+        return EateryMagicRouteRecord::query()
             ->where('resolver_type', $type)
             ->where('raw_location', $rawLocation)
-            ->first();
-
-        if ($record) {
-            return $record;
-        }
-
-        // in pratice we'll never generate a fallback route record on the fly, but only as an admin action
-        // this will later get extracted
-
-        $location = app(ResolveLocationFromStringAction::class)->handle($rawLocation);
-
-        if ( ! $location) {
-            throw new RuntimeException('Location not found');
-        }
-
-        $key = match ($location::class) {
-            EateryCounty::class => 'county_id',
-            EateryTown::class => 'town_id',
-            EateryArea::class => 'area_id',
-        };
-
-        $where = new Where("[parent].{$key}", '=', $location->id);
-
-        $configuration = app(Configuration::class)
-            ->addWhere($where);
-
-        if ($withConfiguration) {
-            $withConfiguration($configuration);
-        }
-
-        return EateryMagicRouteRecord::query()->create([
-            'resolver_type' => $type,
-            'raw_location' => $rawLocation,
-            'location_type' => $location::class,
-            'location_id' => $location->id,
-            'builder_config' => $configuration,
-        ]);
+            ->firstOrFail();
     }
 }
--- tests/Unit/Actions/EatingOut/MagicRouting/FindEateryMagicRouteRecordActionTest.php
+++ tests/Unit/Actions/EatingOut/MagicRouting/FindEateryMagicRouteRecordActionTest.php
@@ -7,6 +7,7 @@ namespace Tests\Unit\Actions\EatingOut\MagicRouting;
 use App\Actions\EatingOut\MagicRouting\FindEateryMagicRouteRecordAction;
 use App\Enums\EatingOut\EateryMagicRouteType;
 use App\Models\EatingOut\EateryMagicRouteRecord;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
@@ -23,5 +24,13 @@ class FindEateryMagicRouteRecordActionTest extends TestCase
         $result = $this->callAction(FindEateryMagicRouteRecordAction::class, EateryMagicRouteType::HundredPercentGlutenFree, 'test-location');
 
         $this->assertSame($record->id, $result->id);
+    }
+
+    #[Test]
+    public function itThrowsIfNoRecordExists(): void
+    {
+        $this->expectException(ModelNotFoundException::class);
+
+        $this->callAction(FindEateryMagicRouteRecordAction::class, EateryMagicRouteType::HundredPercentGlutenFree, 'test-location');
     }
 }
