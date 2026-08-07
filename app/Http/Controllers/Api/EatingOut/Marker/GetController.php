b5ddd3928f561bd7af99cd6cac13df38bd223575
Jamie Peters <jamie@jamie-peters.co.uk>
2026-08-07 21:40:30 +0100

    tests

--- app/Http/Controllers/Api/EatingOut/Marker/GetController.php
+++ app/Http/Controllers/Api/EatingOut/Marker/GetController.php
@@ -6,24 +6,16 @@ namespace App\Http\Controllers\Api\EatingOut\Marker;
 
 use App\Enums\EatingOut\EateryType;
 use App\Services\EatingOut\MarkerGlyphService;
-use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class GetController
 {
-    public function __construct(
-        protected Factory $viewFactory,
-        protected MarkerGlyphService $markerGlyphService,
-    ) {
-        //
-    }
-
-    public function __invoke(Request $request, int $typeId, ?int $venueTypeId = null): Response
+    public function __invoke(Request $request, MarkerGlyphService $markerGlyphService, int $typeId, ?int $venueTypeId = null): Response
     {
-        $marker = $this->viewFactory->make('markers.pin', [
+        $marker = view('markers.pin', [
             'color' => (EateryType::tryFrom($typeId) ?? EateryType::EATERY)->color(),
-            'glyph' => $this->markerGlyphService->resolve($typeId, $venueTypeId),
+            'glyph' => $markerGlyphService->resolve($typeId, $venueTypeId),
         ])->render();
 
         $response = new Response($marker, 200, ['Content-Type' => 'image/svg+xml']);
