e90f85bf84233a6b759019087a342a663bc2f7e7
Jamie Peters <jamie@jamie-peters.co.uk>
2026-08-28 21:27:10 +0100

    Remove recipe highlight

--- resources/js/Components/PageSpecific/Recipes/RecipeMethod.vue
+++ resources/js/Components/PageSpecific/Recipes/RecipeMethod.vue
@@ -7,7 +7,6 @@ const classes = [
   '[&_ol>li]:pl-1',
   '[&_ol>li+li]:mt-3',
   '[&_ol>li::marker]:font-bold [&_ol>li::marker]:text-[1.15em] [&_ol>li::marker]:text-primary-dark',
-  '[&>p]:rounded-sm [&>p]:border-l-[0.35rem] [&>p]:border-secondary [&>p]:bg-primary-lightest [&>p]:p-3 [&>p]:text-[0.95em] [&>p]:text-grey-darker',
 ];
 </script>
 
